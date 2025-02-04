567d73009af2653413e7d90be0e8aa66c0d413e1
Sr-Azrim <1153gfx@gmail.com>
2025-02-04 19:31:33 +0000

    Data_Type

---
+++ .devcontainer/prac/Data_Type.js
@@ -0,0 +1,25 @@
+"use strit";  //treat all js code as newer version
+
+// alert("3 + 3") //we are using nodejs, not browser
+
+console.log(3 +3); console.log("Made on Earth") //code must be readable (heiigh readability)
+
+
+let Name = "Azrim"
+let age = 18
+let isloggedin = false
+
+//number => 2 to power 53
+//bigint 
+//string => " "
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Azrim") //undefined
+console.log(typof "null"); //object
+
